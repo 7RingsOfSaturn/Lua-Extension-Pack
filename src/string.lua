c81ae336f9b86b933e74e3f7926dd28bf36dc718
Andrew <79267815+7RingsOfSaturn@users.noreply.github.com>
2021-02-18 09:58:19 -0600

    Update string.lua

--- src/string.lua
+++ src/string.lua
@@ -1,1 +1,16 @@
+local string = setmetatable({}, {__index = string})
 
+function string.capitalize(String)
+	return String:gsub("^%l", string.upper)
+end
+
+function string.title(String)
+	return String:gsub("%w+", string.capitalize)
+end
+
+function string.trim(String)
+	-- http://lua-users.org/wiki/StringTrim
+	return String:gsub("^%s*(.-)%s*$", "%1")
+end
+
+return string
